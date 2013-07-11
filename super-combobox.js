b6b6a0e11c971627da4627edc19cd8344cb5a7e3
Cédric Félizard <cedric@felizard.fr>
2013-07-11 13:39:36 +0000

    Add demo

---
+++ super-combobox.js
@@ -0,0 +1,91 @@
+// Super Combobox v1.0.0
+
+(function( $ ) {
+  $.widget( "custom.super_combobox", {
+    _create: function() {
+      this.wrapper = $( "<span>" )
+        .addClass( "super-combobox" )
+        .insertAfter( this.element );
+
+      this.element.hide();
+      this._createAutocomplete();
+      this._createShowAllButton();
+    },
+
+    _createAutocomplete: function() {
+      var selected = this.element.children( ":selected" );
+
+      this.input = $( "<input> ")
+        .attr( "id", this.element.attr("id") )
+        .attr( "name", this.element.attr("name") )
+        .attr( "placeholder", this.element.attr("placeholder") )
+        .appendTo( this.wrapper )
+        .addClass( "super-combobox-input ui-widget ui-widget-content ui-state-default ui-corner-left" )
+        .autocomplete({
+          delay: 0,
+          minLength: 0,
+          source: $.proxy( this, "_source" )
+        })
+
+      this._on( this.input, {
+        autocompleteselect: function( event, ui ) {
+          ui.item.option.selected = true;
+          this._trigger( "select", event, {
+            item: ui.item.option
+          });
+        }
+      });
+    },
+
+    _createShowAllButton: function() {
+      var input = this.input,
+        wasOpen = false;
+
+      $( "<a>" )
+        .attr( "tabIndex", -1 )
+        .attr( "title", "Show all items" )
+        .appendTo( this.wrapper )
+        .button({
+          icons: {
+            primary: "ui-icon-triangle-1-s"
+          },
+          text: false
+        })
+        .removeClass( "ui-corner-all" )
+        .addClass( "super-combobox-toggle ui-corner-right" )
+        .mousedown(function() {
+          wasOpen = input.autocomplete( "widget" ).is( ":visible" );
+        })
+        .click(function() {
+          input.focus();
+
+          // Close if already visible
+          if ( wasOpen ) {
+            return;
+          }
+
+          // Pass empty string as value to search for, displaying all results
+          input.autocomplete( "search", "" );
+        });
+    },
+
+    _source: function( request, response ) {
+      var matcher = new RegExp( $.ui.autocomplete.escapeRegex(request.term), "i" );
+      response( this.element.children( "option" ).map(function() {
+        var text = $( this ).text();
+        if ( this.value && ( !request.term || matcher.test(text) ) )
+          return {
+            label: text,
+            value: text,
+            option: this
+          };
+      }) );
+    },
+
+    _destroy: function() {
+      this.wrapper.remove();
+      this.element.show();
+    }
+  });
+})( jQuery );
+
